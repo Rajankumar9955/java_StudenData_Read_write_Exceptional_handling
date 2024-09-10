f7157c975b9fdc677b728a88f5f9591f72a8a5c3
Rajan <krajan92946@gmail.com>
2024-09-10 13:56:44 +0530

    fileHandlingStudentData

--- src/com/example/io/ReadStudentData.java
+++ src/com/example/io/ReadStudentData.java
@@ -1,2 +1,55 @@
-package com.example.io;public class ReadStudentData {
+package com.example.io;
+import com.example.bean.Student;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+public class ReadStudentData {
+    //create a method readData which accept a parameter fileName of type String
+    // and returns an array of Student
+    //create a method readData which accept a parameter fileName of type String
+    // and returns an array of Student
+    public Student[] readData(String fileName) throws IOException {
+        Student[] studentData = null;
+        // create array- size? count number of lines
+        File file = new File(fileName);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        String line = bufferedReader.readLine();
+        int count = 0;
+        while (line != null) {
+            line = bufferedReader.readLine();
+            count++;//1,2,3,4,5
+        }
+
+        studentData = new Student[count - 1];
+        // read data from file, add objects of student to array
+        bufferedReader = new BufferedReader(new FileReader(file));
+        line = bufferedReader.readLine();// reading the header
+        int indexOfStudentData = 0;
+        while (line != null) {
+            line = bufferedReader.readLine();
+            if (line != null) {
+                String[] split = line.split(",");
+                Student student = new Student();
+                student.setName(split[0]);
+                student.setRollNo(Integer.parseInt(split[1]));
+                student.setEmail(split[2]);
+                student.setMobile(split[3]);
+                student.setRank(Integer.parseInt(split[4]));
+
+                studentData[indexOfStudentData] = student;
+                indexOfStudentData++;
+
+               /* String name = split[0];
+                int rollNo = Integer.parseInt(split[1]);
+                String email = split[2];
+                String mobile = split[3];
+                int rank = Integer.parseInt(split[4]);
+                Student student1 = new Student(name, rollNo, email,mobile, rank);*/
+            }
+        }
+        return studentData;
+    }
+
 }
--- src/com/example/main/MyMain.java
+++ src/com/example/main/MyMain.java
@@ -1,2 +1,25 @@
-package com.example.main;public class MyMain {
+package com.example.main;
+
+import com.example.bean.Student;
+import com.example.io.ReadStudentData;
+
+import java.io.IOException;
+public class MyMain {
+    public static void main(String[] args) {
+        ReadStudentData readStudentData = new ReadStudentData();
+        String fileName = "resources/studentdata.csv";
+        try {
+            Student[] students = readStudentData.readData(fileName);
+            for (Student element : students) {
+                if (element.getName().startsWith("M")) {
+                    System.out.println(element);
+                }
+                if (element.getRank() == 1) {
+                    System.out.println(element);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
